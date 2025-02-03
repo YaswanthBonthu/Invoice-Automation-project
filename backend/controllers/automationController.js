fa34067f124a1c619ca3d42f7ed22ee752690ce3
unknown <Balasubrahmanyam@incrivelsoft.com>
2025-02-03 10:59:29 +0530

    Removed secrets from .env

---
+++ backend/controllers/automationController.js
@@ -0,0 +1,9 @@
+const schedule = require('node-schedule');
+
+schedule.scheduleJob('26 19 * * *', async () => {
+    try {
+        
+    } catch (error) {
+        console.log("Error in the ")
+    }
+}
